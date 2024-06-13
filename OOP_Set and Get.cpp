be123a393e0b7f097b3c51cdbc7e997a04ae61b3
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-06-13 08:12:33 +0300

    add the set and the get function to the data member without calling the set and the get directly using the __declspec function.

--- OOP_Set and Get.cpp
+++ OOP_Set and Get.cpp
@@ -11,29 +11,34 @@ private:
 
 public:
 	//Property Set
-	void setFirstName(string FirstName) { _FirstName = FirstName; }
+	void SetFirstName(string FirstName) { _FirstName = FirstName; }
 
 	//Property Get 
-	string FirstName() { return _FirstName; }
+	string GetFirstName() { return _FirstName; }
 
 	//Property Set 
-	void setLastName(string LastName) { _LastName = LastName; }
+	void SetLastName(string LastName) { _LastName = LastName; }
 
 	//Property Get 
-	string LastName() { return _LastName; }
-	string FullName() { return _FirstName + " " + _LastName; }
+	string GetLastName() { return _LastName; }
+	string GetFullName() { return _FirstName + " " + _LastName; }
+
+	__declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+	__declspec(property(get = GetLastName, put = SetLastName)) string LastName;
+
+	__declspec(property(get = GetFullName)) string FullName;
 };
 
 int main() {
 
 		clsPerson Person1;
 
-		Person1.setFirstName("Mohammed");
-		Person1.setLastName("Al-Momany");
+		Person1.FirstName = "Mohammed";
+		Person1.LastName = "Al-Momany";
 
-		cout << "First Name:" << Person1.FirstName() << endl;     
-		cout << "Last Name:" << Person1.LastName() << endl;     
-		cout << "Full Name:" << Person1.FullName() << endl;
+		cout << "First Name:" << Person1.FirstName << endl;     
+		cout << "Last Name:" << Person1.LastName << endl;     
+		cout << "Full Name:" << Person1.FullName << endl;
 
 		system("pause>0"); 
 		return 0;
