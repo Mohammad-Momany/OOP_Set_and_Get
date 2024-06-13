c3de47dc509cc4fa60f98a672768de12f75e5b4e
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-06-13 07:26:17 +0300

    Add example for the get and set Properties

--- OOP_Set and Get.cpp
+++ OOP_Set and Get.cpp
@@ -1,20 +1,40 @@
-// OOP_Set and Get.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
 
-#include <iostream>
+using namespace std; 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+class clsPerson {
+
+private:
+	string _FirstName;
+	string _LastName;
+
+public:
+	//Property Set
+	void setFirstName(string FirstName) { _FirstName = FirstName; }
+
+	//Property Get 
+	string FirstName() { return _FirstName; }
+
+	//Property Set 
+	void setLastName(string LastName) { _LastName = LastName; }
+
+	//Property Get 
+	string LastName() { return _LastName; }
+	string FullName() { return _FirstName + " " + _LastName; }
+};
+
+int main() {
+
+		clsPerson Person1;
+
+		Person1.setFirstName("Mohammed");
+		Person1.setLastName("Al-Momany");
+
+		cout << "First Name:" << Person1.FirstName() << endl;     
+		cout << "Last Name:" << Person1.LastName() << endl;     
+		cout << "Full Name:" << Person1.FullName() << endl;
+
+		system("pause>0"); 
+		return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
